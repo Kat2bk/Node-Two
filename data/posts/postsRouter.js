481f50d594c2aa314ae626894b6119a9a0689fc5
unknown <Katherine2bk@gmail.com>
2021-01-04 16:23:57 -0800

    finished router paths for getting posts, getting posts by id, getting post comments, fairly simple

--- data/posts/postsRouter.js
+++ data/posts/postsRouter.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const Posts = require('../db');
 const router = express.Router();
-const { json } = require('express');
+// const { json } = require('express');
 
 router.get('/', (req, res) => {
     Posts.find()
@@ -13,6 +13,47 @@ router.get('/', (req, res) => {
         console.log(error)
         res.status(500).json({
             error: "The posts information could not be retrieved"
+        })
+    })
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Posts.findById(id)
+    .then(post => {
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        } else {
+            res.json(post)
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({
+            error: "The post information could not be retrieved."
+        })
+    })
+})
+
+//find post comments
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id;
+    Posts.findPostComments(id)
+    .then(comments => {
+        if (!comments) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        } else {
+            res.status(200).json(comments)
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({
+            error: "The comments information could not be retrieved."
         })
     })
 })
